1f29f7ec0d0642e2f726aacbdbffa02136012042
HuyMy <lqhuy91@gmail.com>
2023-11-13 23:39:42 +0700

    Use SavedStateHandle to store data even when app was killed by OS.

--- GeoQuiz/app/src/main/java/com/huymee/android/geoquiz/QuizViewModel.kt
+++ GeoQuiz/app/src/main/java/com/huymee/android/geoquiz/QuizViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
 private const val TAG = "QuizViewModel"
+const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
+const val ANSWERED_COUNT_KEY = "ANSWERED_COUNT_KEY"
+const val CORRECT_COUNT_KEY = "CORRECT_COUNT_KEY"
 
-class QuizViewModel() : ViewModel() {
+class QuizViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -16,9 +19,17 @@ class QuizViewModel() : ViewModel() {
         Question(R.string.question_asia, true)
     )
 
-    private var currentIndex = 0
-    var answeredCount = 0
-    var correctCount = 0
+    private var currentIndex
+        get() = savedStateHandle[CURRENT_INDEX_KEY] ?: 0
+        set(value) = savedStateHandle.set(CURRENT_INDEX_KEY, value)
+
+    var answeredCount
+        get() = savedStateHandle[ANSWERED_COUNT_KEY] ?: 0
+        set(value) = savedStateHandle.set(ANSWERED_COUNT_KEY, value)
+
+    var correctCount
+        get() = savedStateHandle[CORRECT_COUNT_KEY] ?: 0
+        set(value) = savedStateHandle.set(CORRECT_COUNT_KEY, value)
 
     val currentQuestionAnswer: Boolean
         get() = questionBank[currentIndex].answer
