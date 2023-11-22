30a479620a6ae761defc28343bc2a2cdf2bf5845
HuyMy <lqhuy91@gmail.com>
2023-11-22 15:46:10 +0700

    Apply blur effect for Cheat Button.

--- GeoQuiz/app/src/main/java/com/huymee/android/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/huymee/android/geoquiz/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.huymee.android.geoquiz
 
 import android.app.Activity
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import com.huymee.android.geoquiz.databinding.ActivityMainBinding
 
@@ -63,6 +67,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            blurCheatButton()
+        }
     }
 
     private fun updateQuestion() {
@@ -108,5 +116,15 @@ class MainActivity : AppCompatActivity() {
             val resultText = getString(R.string.graded_notice, quizViewModel.score)
             Toast.makeText(this, resultText, Toast.LENGTH_LONG).show()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun blurCheatButton() {
+        val effect = RenderEffect.createBlurEffect(
+            10.0f,
+            10.0f,
+            Shader.TileMode.CLAMP
+        )
+        binding.cheatButton.setRenderEffect(effect)
     }
 }
