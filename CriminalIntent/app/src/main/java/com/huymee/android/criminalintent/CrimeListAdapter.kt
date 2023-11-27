815163278da3a31719aaed13617d8aef24ca0472
HuyMy <lqhuy91@gmail.com>
2023-11-28 00:35:59 +0700

    Add is solved state into list item layout.

--- CriminalIntent/app/src/main/java/com/huymee/android/criminalintent/CrimeListAdapter.kt
+++ CriminalIntent/app/src/main/java/com/huymee/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.huymee.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -34,6 +35,12 @@ class CrimeHolder(
                     "${crime.title} clicked!",
                     Toast.LENGTH_SHORT
                 ).show()
+            }
+
+            crimeSolved.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
             }
         }
     }
