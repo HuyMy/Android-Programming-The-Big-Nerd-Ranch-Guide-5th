3507ddc7cf146dfbf3cc1d5daeb2384cb5149283
HuyMy <lqhuy91@gmail.com>
2023-11-30 23:10:38 +0700

    Implement coroutines to handle heavy tasks.
    //Delay loading crime 5s to simulate a heavy task.

--- CriminalIntent/app/build.gradle.kts
+++ CriminalIntent/app/build.gradle.kts
@@ -39,14 +39,18 @@ android {
 }
 
 dependencies {
-
+    val lifecycleVersion = "2.6.2"
+    val coroutinesVersion = "1.7.3"
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- CriminalIntent/app/src/main/java/com/huymee/android/criminalintent/CrimeListViewModel.kt
+++ CriminalIntent/app/src/main/java/com/huymee/android/criminalintent/CrimeListViewModel.kt
@@ -1,14 +1,31 @@
 package com.huymee.android.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
+private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
 
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.i(TAG, "init starting")
+        viewModelScope.launch {
+            Log.i(TAG, "coroutine launched")
+            crimes += loadCrimes()
+            Log.i(TAG, "Loading crimes finished")
+        }
+
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000L)
         for (i in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
@@ -17,7 +34,8 @@ class CrimeListViewModel : ViewModel() {
                 isSolved = i % 2 == 0
             )
 
-            crimes += crime
+            result += crime
         }
+        return result
     }
 }
