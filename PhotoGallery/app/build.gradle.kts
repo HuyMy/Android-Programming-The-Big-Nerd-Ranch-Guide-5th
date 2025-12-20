fef017f2b863402c37323183b1d338ae15abb517
HuyMy <lqhuy91@gmail.com>
2025-12-20 17:29:26 +0700

    Revert "Executing request to fetch all contents from Flickr and log the result"
    This reverts commit d53ece3474e9386bf6763785128de77d7cc53a9d.

--- PhotoGallery/app/build.gradle.kts
+++ PhotoGallery/app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.retrofit)
-    implementation(libs.converter.scalars)
     implementation(libs.okhttp)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
--- PhotoGallery/app/src/main/java/com/huymee/android/photogallery/api/FlickrApi.kt
+++
@@ -1,8 +0,0 @@
-package com.huymee.android.photogallery.api
-
-import retrofit2.http.GET
-
-interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
-}
