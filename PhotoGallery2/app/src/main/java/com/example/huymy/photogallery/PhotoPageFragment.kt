1917f60add5a1950ec5cdc8004c475978b41ad5d
Huy Le <lqhuy91@gmail.com>
2026-06-07 09:08:33 +0700

    Add app bar layout and progress bar for photo page

--- PhotoGallery2/app/src/main/java/com/example/huymy/photogallery/PhotoPageFragment.kt
+++ PhotoGallery2/app/src/main/java/com/example/huymy/photogallery/PhotoPageFragment.kt
@@ -5,7 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.huymy.photogallery.databinding.FragmentPhotoPageBinding
@@ -25,11 +30,33 @@ class PhotoPageFragment : Fragment() {
             false
         )
         binding.apply {
+            progressBar.max = 100
+
             webView.apply {
                 settings.javaScriptEnabled = true
                 webViewClient = WebViewClient()
                 loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                        if (newProgress == 100) {
+                            progressBar.visibility = View.GONE
+                        } else {
+                            progressBar.visibility = View.VISIBLE
+                            progressBar.progress = newProgress
+                        }
+                    }
+
+                    override fun onReceivedTitle(view: WebView?, title: String?) {
+                        binding.fragmentPageActionBar.subtitle = title
+                    }
+                }
             }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(binding.fragmentPageActionBar) { view, insets ->
+            val statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            view.updatePadding(top = statusBarHeight)
+            insets
         }
         return binding.root
     }
